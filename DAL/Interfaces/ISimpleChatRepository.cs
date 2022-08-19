9e2df22260a5ecc3c069d21bc6332b84f53d27ce
Andrii Tsylia <andrii.tsylia@outlook.com>
2022-08-19 12:22:13 +0300

    Added repository

---
+++ DAL/Interfaces/ISimpleChatRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface ISimpleChatRepository<T> where T : class
+    {
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        bool SaveChanges();
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Get(Expression<Func<T, bool>>? filter,
+                           Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy,
+                           string includeProperties = "");
+    }
+}
---
+++ DAL/Repositories/SimpleChatRepository.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Interfaces;
+using DAL.EF;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace DAL.Repositories
+{
+    public class SimpleChatRepository<T> : ISimpleChatRepository<T> where T : class
+    {
+        private readonly ChatContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public SimpleChatRepository(ChatContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public void Create(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            _dbSet.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            _dbSet.Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            _dbSet.Remove(entity);
+        }
+
+        public bool SaveChanges()
+        {
+            return _context.SaveChanges() >= 0;
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Get(Expression<Func<T, bool>>? filter,
+                                  Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy,
+                                  string includeProperties = "")
+        {
+            IQueryable<T> query = _dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split(',', StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperties);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+    }
+}
